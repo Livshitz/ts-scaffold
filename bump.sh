b3bd3ec4d73729970e82df2beddb31d6a3b8471f
Livshitz <elya.livshitz@gmail.com>
2020-06-05 00:23:37 +0300

    .

--- bump.sh
+++ bump.sh
@@ -1,7 +1,8 @@
 set -e && cd `pwd`
 
 yarn publish . --non-interactive --tag latest 
-git push --tags 
+git tag $npm_package_version
+git push origin --tags 
 
 yarn version --patch
 readonly VERSION=$(< package.json grep version \
@@ -10,5 +11,6 @@ readonly VERSION=$(< package.json grep version \
   | sed 's/[",]//g' \
   | tr -d '[:space:]')
 git commit --amend -m "^ pkg bump ($VERSION) [skip-ci]" 
+git push origin
 
 echo \"Successfully released version $npm_package_version!\"
