9f7387324b95061ab93afa42c339f599b9627cf8
Elya Livshitz <elya.livshitz@gmail.com>
2021-12-16 23:06:19 +0200

    + `Module` template file

---
+++ src/Module.ts
@@ -0,0 +1,7 @@
+export class Module {
+    public constructor(public options?: Partial<ModuleOptions>) {
+        this.options = { ...new ModuleOptions(), ...options };
+    }
+}
+
+export class ModuleOptions {}
